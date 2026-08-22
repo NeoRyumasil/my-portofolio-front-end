110116ec5c9231616484c29ec361ded0ae796517
Alvin Ababil <neoryumasil@gmail.com>
2026-08-22 21:00:18 +0700

    Biggens Badge Role

--- portofolio-front-end/components/sections/developer/About.tsx
+++ portofolio-front-end/components/sections/developer/About.tsx
@@ -5,8 +5,8 @@ export default function About() {
     <section id="about" className="flex flex-col-reverse md:flex-row items-center justify-between gap-12 pt-10 transition-colors duration-300">
       <div className="flex-1 space-y-6">
 
-        <div className="inline-flex items-center gap-2 bg-[#7DD3FC]/20 dark:bg-[#E11D48]/20 text-[#0369A1] dark:text-[#F43F5E] px-3 py-1.5 rounded-full text-xs font-bold tracking-widest font-space">
-          <span className="w-2 h-2 rounded-full bg-[#0369A1] dark:bg-[#F43F5E]"></span>
+        <div className="inline-flex items-center gap-3 bg-[#7DD3FC]/20 dark:bg-[#E11D48]/20 text-[#0369A1] dark:text-[#F43F5E] px-5 py-2.5 rounded-full text-sm md:text-base font-bold tracking-widest font-space shadow-sm">
+          <span className="w-3 h-3 rounded-full bg-[#0369A1] dark:bg-[#F43F5E] animate-pulse"></span>
           Web Backend and Game Developer
         </div>
 
